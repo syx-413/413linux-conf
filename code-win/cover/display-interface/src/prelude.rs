8574c820e76a992c94647c7c4511118f2f4b397c
LS <the413reply@gmail.com>
2025-01-30 04:14:06 +0000

    remove code

--- code-win/cover/display-interface/src/prelude.rs
+++
@@ -1,3 +0,0 @@
-pub use crate::AsyncWriteOnlyDataCommand as _display_interface_AsyncWriteOnlyDataCommand;
-pub use crate::DisplayError as _display_interface_DisplayError;
-pub use crate::WriteOnlyDataCommand as _display_interface_WriteOnlyDataCommand;
